55b2a57e76f4f765c4e71d53c6d832511c9b34af
jaxarthur <jaxarthur@outlook.com>
2019-09-13 09:46:33 -0600

    Add Prototype 1

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int forMagnatude = 10;
+    public float turnMagnatude = .3f;
+    private float horizInput;
+    private float vertInput;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        vertInput = Input.GetAxis("Vertical");
+        horizInput = Input.GetAxis("Horizontal");
+        //Move Car Forward
+        rb.velocity = (Vector3.forward * forMagnatude * vertInput);
+        rb.angularVelocity = (Vector3.up * turnMagnatude * horizInput);
+    }
+}
