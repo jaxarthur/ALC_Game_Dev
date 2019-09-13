ad6e490f5362b10957ebf79d173f960fda74929c
jaxarthur <jaxarthur@outlook.com>
2019-09-13 10:28:25 -0600

    InitItAlready

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public int forMagnatude = 1000;
+    public int turnMagnatude = 1000;
+    private float horizInput;
+    private float vertInput;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        vertInput = Input.GetAxis("Vertical");
+        horizInput = Input.GetAxis("Horizontal");
+        //Move Car Forward
+        rb.AddRelativeForce(Vector3.forward * forMagnatude * vertInput);
+        rb.AddRelativeTorque(Vector3.up * turnMagnatude * horizInput);
+    }
+}
