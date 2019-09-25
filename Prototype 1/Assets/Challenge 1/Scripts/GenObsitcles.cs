55808837098de6757f2f3ffd59c9b4e33958553d
jaxarthur <jaxarthur@outlook.com>
2019-09-25 09:54:27 -0600

    Proper Gen

--- Prototype 1/Assets/Challenge 1/Scripts/GenObsitcles.cs
+++ Prototype 1/Assets/Challenge 1/Scripts/GenObsitcles.cs
@@ -10,22 +10,24 @@ public class GenObsitcles : MonoBehaviour
     public int dist = 50;
     public int space = 10;
     public int amount = 10;
+    public int begin = 50;
+    public int range = 25;
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < amount; i++)
         {
-            int hole = (int)Random.Range(-100f, 100f);
+            int hole = (int)Random.Range(range * -1, range);
             int topBottom = hole + space;
             int bottomTop = hole - space;
             int topMiddle = (100 + topBottom) / 2;
             int bottomMiddle = (-100 + bottomTop) / 2;
 
-            topClone = Instantiate(Obs, new Vector3(0, topMiddle, i * dist), Quaternion.AngleAxis(0,Vector3.forward));
-            bottomClone = Instantiate(Obs, new Vector3(0, bottomMiddle, i * dist), Quaternion.AngleAxis(0, Vector3.forward));
+            topClone = Instantiate(Obs, new Vector3(0, topMiddle, i * dist + begin), Quaternion.AngleAxis(0,Vector3.forward));
+            bottomClone = Instantiate(Obs, new Vector3(0, bottomMiddle, i * dist + begin), Quaternion.AngleAxis(0, Vector3.forward));
 
-            topClone.transform.localScale = new Vector3(100, Mathf.Abs(topBottom - topMiddle), 1);
-            bottomClone.transform.localScale = new Vector3(100, Mathf.Abs(bottomTop - bottomMiddle), 1);
+            topClone.transform.localScale = new Vector3(100, Mathf.Abs(topBottom - topMiddle) * 2, 1);
+            bottomClone.transform.localScale = new Vector3(100, Mathf.Abs(bottomTop - bottomMiddle) * 2, 1);
 
         }
     }
